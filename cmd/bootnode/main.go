7dda673bca5e749d9340c1505f0c6afa23163ab2
webchain-network <webchainabc@gmail.com>
2018-04-26 10:39:28 +0200

    Fix bootstrap nodes

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -35,7 +35,7 @@ import (
 var Version = "unknown"
 
 var (
-	listenAddr  = flag.String("addr", ":30301", "listen address")
+	listenAddr  = flag.String("addr", ":30335", "listen address")
 	genKey      = flag.String("genkey", "", "generate a node key and quit")
 	nodeKeyFile = flag.String("nodekey", "", "private key filename")
 	nodeKeyHex  = flag.String("nodekeyhex", "", "private key as hex (for testing)")
