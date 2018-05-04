44904c626eab14aba67fdf0a687ab41b15479f96
webchain-network <webchainabc@gmail.com>
2018-05-04 14:19:28 +0200

    Added detection of incompatible nodes

--- cmd/webchaind/flags.go
+++ cmd/webchaind/flags.go
@@ -332,7 +332,7 @@ var (
 	ListenPortFlag = cli.IntFlag{
 		Name:  "port",
 		Usage: "Network listening port",
-		Value: 31339,
+		Value: 31340,
 	}
 	BootnodesFlag = cli.StringFlag{
 		Name:  "bootnodes",
